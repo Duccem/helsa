4a8dc944471aeec8962633fc736a27c5411b4899
Jose Veliz <ducen29@gmail.com>
2026-03-12 23:05:05 -0400

    feat(dashboard): add HomePage and DashboardLayout components for user dashboard

---
+++ src/app/(app)/(dashboard)/home/page.tsx
@@ -0,0 +1,18 @@
+import { requireAuth } from "@/modules/auth/infrastructure/guards/require-auth";
+
+export default async function HomePage() {
+  await requireAuth();
+  return (
+    <div className="flex flex-col gap-6 w-2/3">
+      <div className="space-y-2 text-center">
+        <h2 className="text-3xl text-foreground">Welcome to your dashboard</h2>
+        <p className="text-muted-foreground">
+          This is your dashboard where you can manage your account, view your activity, and access exclusive features.
+          Use the navigation menu to explore different sections of your dashboard. If you have any questions or need
+          assistance, feel free to contact our support team.
+        </p>
+      </div>
+    </div>
+  );
+}
+
